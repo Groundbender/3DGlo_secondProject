478af021ca5b2f4b8f99c46f795bc98744091e1c
Damir <damir24may@mail.ru>
2023-02-24 19:45:47 +0300

    fixed js build

---
+++ src/modules/anchors.js
@@ -0,0 +1,26 @@
+const anchorsAnimate = () => {
+  const menu = document.querySelector("menu");
+  let menuItems = menu.querySelectorAll('ul>li>a[href*="#"]');
+  const skipBtn = document.querySelector("a[href='#service-block']");
+  console.log(skipBtn);
+  menuItems.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      let blockID = link.getAttribute("href");
+      console.log(blockID);
+      document.querySelector("" + blockID).scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    });
+  });
+  skipBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.getElementById("service-block").scrollIntoView({
+      block: "start",
+      behavior: "smooth",
+    });
+  });
+};
+
+export default anchorsAnimate;
---
+++ src/modules/modal.js
@@ -0,0 +1,55 @@
+const modal = () => {
+  const modal = document.querySelector(".popup");
+  const buttons = document.querySelectorAll(".popup-btn");
+  const closeBtn = modal.querySelector(".popup-close");
+  const modalDialog = document.querySelector(".popup-content");
+
+  let start = -500;
+  let finish = 0;
+  let position = -500;
+  let count = 5;
+  let idPopup;
+
+  // modalDialog.style.transform = `translateY(-100%) translateX(-50px)`;
+
+  const showAnimation = () => {
+    if (start < finish) {
+      position += count;
+    } else if (start > finish) {
+      position -= count;
+    }
+    modalDialog.style.transform = `translateY(${position}px) translateX(-50px)`;
+    let condition;
+    if (start > finish) {
+      condition = position > finish;
+    } else {
+      condition = position < finish;
+    }
+    if (condition) {
+      idPopup = requestAnimationFrame(showAnimation);
+    }
+    // if (start > finish ? position > finish : position < finish) {
+    // idPopup = requestAnimationFrame(showAnimation);
+    // }
+
+    // idPopup = requestAnimationFrame(showAnimation);
+  };
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+      if (screen.width > 768) {
+        position = start;
+        idPopup = requestAnimationFrame(showAnimation);
+      }
+      console.log(start);
+      console.log(finish);
+      console.log(position);
+      console.log(count);
+    });
+  });
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+};
+
+export default modal;
