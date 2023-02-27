927e1d1a78336147612d4bcab59e9c6d25514180
Damir <damir24may@mail.ru>
2023-02-27 19:40:53 +0300

    validation fixed

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -6,6 +6,7 @@ const validation = () => {
   const mainForm = document.querySelectorAll("form")[0];
   const footerForm = document.querySelectorAll("form")[1];
   const modalForm = document.querySelectorAll("form")[2];
+  let messageArea = footerForm.querySelector("input.mess");
 
   calcInputs.forEach((input) => {
     input.addEventListener("input", (e) => {
@@ -13,116 +14,57 @@ const validation = () => {
     });
   });
 
-  let inputNameMain = mainForm.querySelector("input[type='text']");
-  let inputMailMain = mainForm.querySelector("input[type='email']");
-  let inputTelMain = mainForm.querySelector("input[type='tel']");
+  forms.forEach((form) => {
+    let inputName = form.querySelector("input[type='text']");
+    let inputMail = form.querySelector("input[type='email']");
+    let inputTel = form.querySelector("input[type='tel']");
 
-  mainForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let isError = false;
-    if (
-      !/[^а-яА-Я\s\-]/g.test(inputNameMain.value) &&
-      inputNameMain.value !== ""
-    ) {
-      console.log("Имя валидино");
-    } else {
-      isError = true;
-    }
-    if (
-      !/[^a-zA-Z\d\@\-\_\.\!\~\*\']/g.test(inputMailMain.value) &&
-      inputMailMain.value !== ""
-    ) {
-      console.log("email валиден");
-    } else {
-      isError = true;
-    }
-    if (!/[^\d\(\)\-]/g.test(inputTelMain.value) && inputTelMain.value !== "") {
-      console.log("tel валиден");
-    } else {
-      isError = true;
-    }
+    inputName.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\s\-]/g, "");
+    });
+    inputName.addEventListener("blur", (e) => {
+      e.target.value = e.target.value.replace(/^[\s\-]+/g, "");
+      e.target.value = e.target.value.replace(/[\s\-]+$/g, "");
+      e.target.value = e.target.value.replace(/\s{2,}/g, " ");
+      e.target.value = e.target.value.replace(/\-{2,}/g, "-");
+      if (e.target.value) {
+        e.target.value =
+          e.target.value[0].toUpperCase() +
+          e.target.value.slice(1).toLowerCase();
+      }
+    });
 
-    if (!isError) {
-      console.log("Данные отправлены");
-    }
-  });
-  let inputNameFooter = footerForm.querySelector("input[type='text']");
-  let inputMailFooter = footerForm.querySelector("input[type='email']");
-  let inputTelFooter = footerForm.querySelector("input[type='tel']");
-  let messageArea = footerForm.querySelector("input.mess");
-
-  footerForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let isError = false;
-    if (
-      !/[^а-яА-Я\s\-]/g.test(inputNameFooter.value) &&
-      inputNameFooter.value !== ""
-    ) {
-      console.log("Имя валидино");
-    } else {
-      isError = true;
-    }
-    if (
-      !/[^a-zA-Z\d\@\-\_\.\!\~\*\']/g.test(inputMailFooter.value) &&
-      inputMailFooter.value !== ""
-    ) {
-      console.log("email валиден");
-    } else {
-      isError = true;
-    }
-    if (
-      !/[^\d\(\)\-]/g.test(inputTelFooter.value) &&
-      inputTelFooter.value !== ""
-    ) {
-      console.log("tel валиден");
-    } else {
-      isError = true;
-    }
-    if (!/[^а-яА-Я\s\-]/g.test(messageArea.value) && messageArea.value !== "") {
-      console.log("текст валиден");
-    } else {
-      isError = true;
-    }
-
-    if (!isError) {
-      console.log("Данные отправлены");
-    }
+    inputMail.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(
+        /[^a-zA-Z\d\@\-\_\.\!\~\*\']/g,
+        ""
+      );
+    });
+    inputMail.addEventListener("blur", (e) => {
+      e.target.value = e.target.value.replace(/^[\-]+/g, "");
+      e.target.value = e.target.value.replace(/[\-]+$/g, "");
+      e.target.value = e.target.value.replace(/\s{2,}/g, " ");
+      e.target.value = e.target.value.replace(/\-{2,}/g, "-");
+    });
+    inputTel.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^\d\(\)\-]/g, "");
+    });
+    inputTel.addEventListener("blur", (e) => {
+      e.target.value = e.target.value.replace(/^[\-]+/g, "");
+      e.target.value = e.target.value.replace(/[\-]+$/g, "");
+      e.target.value = e.target.value.replace(/\s{2,}/g, " ");
+      e.target.value = e.target.value.replace(/\-{2,}/g, "-");
+    });
   });
 
-  let inputNameModal = modalForm.querySelector("input[type='text']");
-  let inputMailModal = modalForm.querySelector("input[type='email']");
-  let inputTelModal = modalForm.querySelector("input[type='tel']");
-  modalForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let isError = false;
-    if (
-      !/[^а-яА-Я\s\-]/g.test(inputNameModal.value) &&
-      inputNameModal.value !== ""
-    ) {
-      console.log("Имя валидино");
-    } else {
-      isError = true;
-    }
-    if (
-      !/[^a-zA-Z\d\@\-\_\.\!\~\*\']/g.test(inputMailModal.value) &&
-      inputMailModal.value !== ""
-    ) {
-      console.log("email валиден");
-    } else {
-      isError = true;
-    }
-    if (
-      !/[^\d\(\)\-]/g.test(inputTelModal.value) &&
-      inputTelModal.value !== ""
-    ) {
-      console.log("tel валиден");
-    } else {
-      isError = true;
-    }
-
-    if (!isError) {
-      console.log("Данные отправлены");
-    }
+  messageArea.addEventListener("input", (event) => {
+    event.target.value = event.target.value.replace(/[^а-яА-ЯёЁ\s\-]/g, "");
+  });
+  messageArea.addEventListener("blur", (e) => {
+    e.target.value = e.target.value.replace(/^[\s\-]+/g, "");
+    e.target.value = e.target.value.replace(/[\s\-]+$/g, "");
+    e.target.value = e.target.value.replace(/\s{2,}/g, " ");
+    e.target.value = e.target.value.replace(/\-{2,}/g, "-");
   });
 };
 
