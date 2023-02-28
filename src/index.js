568a3ec6e6d86f80e9d96266e4309fa98b5d679d
Damir <damir24may@mail.ru>
2023-02-28 23:45:02 +0300

    added slider and dots js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import modal from "./modules/modal";
 import anchorsAnimate from "./modules/anchors";
 import validation from "./modules/validation";
 import tabs from "./modules/tabs";
-
+import slider from "./modules/slider";
 timer("1 marth 2023");
 menu();
 modal();
 anchorsAnimate();
 validation();
 tabs();
+slider();
